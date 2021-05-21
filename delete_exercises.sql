a53e9028c66454bd21bde0f588561272d38ac899
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-20 23:02:23 -0500

    feat: completed delete_exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,26 @@
 USE codeup_test_db;
 
-# SELECT release_date
-# FROM albums
-# WHERE release_date >= 1991;
-#
-# SELECT genre
-# FROM albums
-# WHERE genre ='disco';
+SELECT release_date
+FROM albums
+WHERE release_date >= 1991;
+
+SELECT genre
+FROM albums
+WHERE genre ='disco';
 
 SELECT record_name
 FROM albums
-WHERE artist_name = 'Whitney Houston';
+WHERE artist_name = 'Meat Loaf';
+
+SELECT * FROM albums;
+
+DELETE FROM albums
+WHERE release_date >= 1991;
+
+DELETE FROM albums
+WHERE genre ='disco';
+
+DELETE FROM albums
+WHERE artist_name = 'Meat Loaf';
+
+SELECT * FROM albums;
