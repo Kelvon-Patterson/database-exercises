b09bb916d9d51ad54ddf5c870746b7b602945371
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-24 22:11:04 -0500

    feat: completing subqueries_exercises for practice

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -4,7 +4,7 @@ USE employees;
 # FROM employees
 # WHERE hire_date = (SELECT hire_date FROM employees where emp_no = 101010);
 #
-# SELECT title
+# SELECT DISTINCT title
 # FROM titles
 # WHERE emp_no IN
 # (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
@@ -14,18 +14,36 @@ USE employees;
 # WHERE emp_no IN
 # (SELECT emp_no FROM dept_manager WHERE to_date > now())
 # AND gender = 'F';
-
+#
 # SELECT dept_name
 # FROM departments
 # WHERE dept_no IN
 # (SELECT dept_no FROM dept_manager WHERE emp_no IN
 #     (SELECT emp_no FROM employees WHERE  gender = 'F')
 #     AND to_date > NOW());
+#
+# SELECT first_name, last_name
+# FROM employees
+# WHERE emp_no IN
+#     (SELECT emp_no FROM salaries WHERE salary IN
+#         (SELECT MAX(salary)
+#         FROM salaries ));
 
-SELECT first_name, last_name
+
+
+
+
+#at home practice
+
+SELECT emp_no, hire_date,first_name,last_name
 FROM employees
+WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = 101010);
+
+SELECT DISTINCT title
+FROM titles
 WHERE emp_no IN
-    (SELECT emp_no FROM salaries WHERE salary IN
-        (SELECT MAX(salary)
-        FROM salaries ));
+(SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+
+
 
