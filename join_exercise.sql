b256f7af020c98b33eedfac60bf31c0b07dd5008
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-26 21:38:26 -0500

    feat: completed exercise #1

--- join_exercise.sql
+++ join_exercise.sql
@@ -17,16 +17,24 @@ ON dept_manager.dept_no = departments.dept_no
 WHERE dept_manager.to_date > CURDATE()
 AND employees.gender = 'F';
 
-SELECT titles.title  , COUNT(*) AS 'Total'
-FROM titles
-JOIN dept_emp
-ON dept_emp.emp_no = titles.emp_no
-JOIN departments
-ON dept_emp.dept_no = departments.dept_no
-WHERE dept_name = 'Customer Service'
-AND dept_emp.to_date ='9999-01-01'
-GROUP BY titles.title;
+# SELECT titles.title  , COUNT(title) AS 'Total'
+# FROM titles
+# JOIN dept_emp
+# ON dept_emp.emp_no = titles.emp_no
+# JOIN departments
+# ON dept_emp.dept_no = departments.dept_no
+# WHERE dept_name = 'Customer Service'
+# AND dept_emp.to_date ='9999-01-01'
+# GROUP BY titles.title;
 
+SELECT t.title, COUNT(t.title) AS 'Total'
+FROM titles t
+         JOIN dept_emp de
+              ON t.emp_no = de.emp_no
+         JOIN departments d
+              ON de.dept_no = d.dept_no
+WHERE t.to_date = '9999-01-01' AND d.dept_name = 'Customer Service'
+GROUP BY t.title;
 
 
 
